8fba6e4d62cb6217a7d9ed8fe16e0d1818b07ec2
siyuan9211 <jerry9211@163.com>
2025-08-13 12:53:48 +1000

    SpringBoot 框架

---
+++ src/test/java/org/example/elec5619_practical03_group_7/Elec5619Practical03Group7ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.elec5619_practical03_group_7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Elec5619Practical03Group7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
