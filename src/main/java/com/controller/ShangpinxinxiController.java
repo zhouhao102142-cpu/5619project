97844d40e90e6af9b975aa440a107d5b1d27a238
siyuan9211 <jerry9211@163.com>
2025-10-08 16:40:51 +1100

    feat: Add commodities and merchants functionality

---
+++ src/main/java/com/controller/ShangpinxinxiController.java
@@ -0,0 +1,322 @@
+package com.controller;
+
+import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Date;
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+
+import com.utils.ValidatorUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
+import com.baomidou.mybatisplus.mapper.Wrapper;
+import com.annotation.IgnoreAuth;
+import com.entity.OrdersEntity;
+import com.service.OrdersService;
+import com.utils.UserBasedCollaborativeFiltering;
+
+import com.entity.ShangpinxinxiEntity;
+import com.entity.view.ShangpinxinxiView;
+
+import com.service.ShangpinxinxiService;
+import com.service.TokenService;
+import com.utils.PageUtils;
+import com.utils.R;
+import com.utils.MPUtil;
+import com.utils.MapUtils;
+import com.utils.CommonUtil;
+import java.io.IOException;
+import com.service.StoreupService;
+import com.entity.StoreupEntity;
+
+/**
+ * 图书信息
+ * 后端接口
+ * @author 
+ * @email 
+ * @date 2024-05-15 17:12:00
+ */
+@RestController
+@RequestMapping("/shangpinxinxi")
+public class ShangpinxinxiController {
+    @Autowired
+    private ShangpinxinxiService shangpinxinxiService;
+
+    @Autowired
+    private StoreupService storeupService;
+
+    @Autowired
+    private OrdersService ordersService;
+
+
+    
+
+
+
+    /**
+     * 后端列表
+     */
+    @RequestMapping("/page")
+    public R page(@RequestParam Map<String, Object> params,ShangpinxinxiEntity shangpinxinxi,
+                @RequestParam(required = false) Double pricestart,
+                @RequestParam(required = false) Double priceend,
+		HttpServletRequest request){
+		String tableName = request.getSession().getAttribute("tableName").toString();
+		if(tableName.equals("shangjia")) {
+			shangpinxinxi.setShangjiazhanghao((String)request.getSession().getAttribute("username"));
+		}
+        EntityWrapper<ShangpinxinxiEntity> ew = new EntityWrapper<ShangpinxinxiEntity>();
+                if(pricestart!=null) ew.ge("price", pricestart);
+                if(priceend!=null) ew.le("price", priceend);
+
+		PageUtils page = shangpinxinxiService.queryPage(params, MPUtil.sort(MPUtil.between(MPUtil.likeOrEq(ew, shangpinxinxi), params), params));
+
+        return R.ok().put("data", page);
+    }
+    
+    /**
+     * 前端列表
+     */
+	@IgnoreAuth
+    @RequestMapping("/list")
+    public R list(@RequestParam Map<String, Object> params,ShangpinxinxiEntity shangpinxinxi, 
+                @RequestParam(required = false) Double pricestart,
+                @RequestParam(required = false) Double priceend,
+		HttpServletRequest request){
+        EntityWrapper<ShangpinxinxiEntity> ew = new EntityWrapper<ShangpinxinxiEntity>();
+                if(pricestart!=null) ew.ge("price", pricestart);
+                if(priceend!=null) ew.le("price", priceend);
+
+		PageUtils page = shangpinxinxiService.queryPage(params, MPUtil.sort(MPUtil.between(MPUtil.likeOrEq(ew, shangpinxinxi), params), params));
+        return R.ok().put("data", page);
+    }
+
+
+
+	/**
+     * 列表
+     */
+    @RequestMapping("/lists")
+    public R list( ShangpinxinxiEntity shangpinxinxi){
+       	EntityWrapper<ShangpinxinxiEntity> ew = new EntityWrapper<ShangpinxinxiEntity>();
+      	ew.allEq(MPUtil.allEQMapPre( shangpinxinxi, "shangpinxinxi")); 
+        return R.ok().put("data", shangpinxinxiService.selectListView(ew));
+    }
+
+	 /**
+     * 查询
+     */
+    @RequestMapping("/query")
+    public R query(ShangpinxinxiEntity shangpinxinxi){
+        EntityWrapper< ShangpinxinxiEntity> ew = new EntityWrapper< ShangpinxinxiEntity>();
+ 		ew.allEq(MPUtil.allEQMapPre( shangpinxinxi, "shangpinxinxi")); 
+		ShangpinxinxiView shangpinxinxiView =  shangpinxinxiService.selectView(ew);
+		return R.ok("查询图书信息成功").put("data", shangpinxinxiView);
+    }
+	
+    /**
+     * 后端详情
+     */
+    @RequestMapping("/info/{id}")
+    public R info(@PathVariable("id") Long id){
+        ShangpinxinxiEntity shangpinxinxi = shangpinxinxiService.selectById(id);
+		shangpinxinxi.setClicknum(shangpinxinxi.getClicknum()+1);
+		shangpinxinxiService.updateById(shangpinxinxi);
+        shangpinxinxi = shangpinxinxiService.selectView(new EntityWrapper<ShangpinxinxiEntity>().eq("id", id));
+        return R.ok().put("data", shangpinxinxi);
+    }
+
+    /**
+     * 前端详情
+     */
+	@IgnoreAuth
+    @RequestMapping("/detail/{id}")
+    public R detail(@PathVariable("id") Long id){
+        ShangpinxinxiEntity shangpinxinxi = shangpinxinxiService.selectById(id);
+		shangpinxinxi.setClicknum(shangpinxinxi.getClicknum()+1);
+		shangpinxinxiService.updateById(shangpinxinxi);
+        shangpinxinxi = shangpinxinxiService.selectView(new EntityWrapper<ShangpinxinxiEntity>().eq("id", id));
+        return R.ok().put("data", shangpinxinxi);
+    }
+    
+
+
+    /**
+     * 赞或踩
+     */
+    @RequestMapping("/thumbsup/{id}")
+    public R vote(@PathVariable("id") String id,String type){
+        ShangpinxinxiEntity shangpinxinxi = shangpinxinxiService.selectById(id);
+        if(type.equals("1")) {
+        	shangpinxinxi.setThumbsupnum(shangpinxinxi.getThumbsupnum()+1);
+        } else {
+        	shangpinxinxi.setCrazilynum(shangpinxinxi.getCrazilynum()+1);
+        }
+        shangpinxinxiService.updateById(shangpinxinxi);
+        return R.ok("投票成功");
+    }
+
+    /**
+     * 后端保存
+     */
+    @RequestMapping("/save")
+    public R save(@RequestBody ShangpinxinxiEntity shangpinxinxi, HttpServletRequest request){
+    	//ValidatorUtils.validateEntity(shangpinxinxi);
+        shangpinxinxiService.insert(shangpinxinxi);
+        return R.ok();
+    }
+    
+    /**
+     * 前端保存
+     */
+    @RequestMapping("/add")
+    public R add(@RequestBody ShangpinxinxiEntity shangpinxinxi, HttpServletRequest request){
+    	//ValidatorUtils.validateEntity(shangpinxinxi);
+        shangpinxinxiService.insert(shangpinxinxi);
+        return R.ok();
+    }
+
+
+
+
+
+    /**
+     * 修改
+     */
+    @RequestMapping("/update")
+    @Transactional
+    public R update(@RequestBody ShangpinxinxiEntity shangpinxinxi, HttpServletRequest request){
+        //ValidatorUtils.validateEntity(shangpinxinxi);
+        shangpinxinxiService.updateById(shangpinxinxi);//全部更新
+        return R.ok();
+    }
+
+
+
+    
+
+    /**
+     * 删除
+     */
+    @RequestMapping("/delete")
+    public R delete(@RequestBody Long[] ids){
+        shangpinxinxiService.deleteBatchIds(Arrays.asList(ids));
+        return R.ok();
+    }
+    
+	
+	/**
+     * 前端智能排序
+     */
+	@IgnoreAuth
+    @RequestMapping("/autoSort")
+    public R autoSort(@RequestParam Map<String, Object> params,ShangpinxinxiEntity shangpinxinxi, HttpServletRequest request,String pre){
+        EntityWrapper<ShangpinxinxiEntity> ew = new EntityWrapper<ShangpinxinxiEntity>();
+        Map<String, Object> newMap = new HashMap<String, Object>();
+        Map<String, Object> param = new HashMap<String, Object>();
+		Iterator<Map.Entry<String, Object>> it = param.entrySet().iterator();
+		while (it.hasNext()) {
+			Map.Entry<String, Object> entry = it.next();
+			String key = entry.getKey();
+			String newKey = entry.getKey();
+			if (pre.endsWith(".")) {
+				newMap.put(pre + newKey, entry.getValue());
+			} else if (StringUtils.isEmpty(pre)) {
+				newMap.put(newKey, entry.getValue());
+			} else {
+				newMap.put(pre + "." + newKey, entry.getValue());
+			}
+		}
+		params.put("sort", "clicknum");
+        params.put("order", "desc");
+		PageUtils page = shangpinxinxiService.queryPage(params, MPUtil.sort(MPUtil.between(MPUtil.likeOrEq(ew, shangpinxinxi), params), params));
+        return R.ok().put("data", page);
+    }
+
+
+    /**
+     * 协同算法（基于用户的协同算法）
+     */
+    @RequestMapping("/autoSort2")
+    public R autoSort2(@RequestParam Map<String, Object> params,ShangpinxinxiEntity shangpinxinxi, HttpServletRequest request){
+        String userId = request.getSession().getAttribute("userId").toString();
+        Integer limit = params.get("limit")==null?10:Integer.parseInt(params.get("limit").toString());
+        List<OrdersEntity> orders = ordersService.selectList(new EntityWrapper<OrdersEntity>());
+        Map<String, Map<String, Double>> ratings = new HashMap<>();
+        if(orders!=null && orders.size()>0) {
+            for(OrdersEntity o : orders) {
+                Map<String, Double> userRatings = null;
+                if(ratings.containsKey(o.getUserid().toString())) {
+                    userRatings = ratings.get(o.getUserid().toString());
+                } else {
+                    userRatings = new HashMap<>();
+                    ratings.put(o.getUserid().toString(), userRatings);
+                }
+                if(userRatings.containsKey(o.getGoodid().toString())) {
+                    userRatings.put(o.getGoodid().toString(), userRatings.get(o.getGoodid().toString())+1.0);
+                } else {
+                    userRatings.put(o.getGoodid().toString(), 1.0);
+                }
+
+            }
+        }
+        // 创建协同过滤对象
+        UserBasedCollaborativeFiltering filter = new UserBasedCollaborativeFiltering(ratings);
+
+        // 为指定用户推荐物品
+        String targetUser = userId;
+        int numRecommendations = limit;
+        List<String> recommendations = filter.recommendItems(targetUser, numRecommendations);
+
+        // 输出推荐结果
+        System.out.println("Recommendations for " + targetUser + ":");
+        for (String item : recommendations) {
+            System.out.println(item);
+        }
+
+        EntityWrapper<ShangpinxinxiEntity> ew = new EntityWrapper<ShangpinxinxiEntity>();
+        ew.in("id", recommendations);
+        if(recommendations!=null && recommendations.size()>0) {
+            ew.last("order by FIELD(id, "+String.join(",", recommendations)+")");
+        }
+
+        PageUtils page = shangpinxinxiService.queryPage(params, ew);
+        List<ShangpinxinxiEntity> pageList = (List<ShangpinxinxiEntity>)page.getList();
+        if(pageList.size()<limit) {
+            int toAddNum = limit-pageList.size();
+            ew = new EntityWrapper<ShangpinxinxiEntity>();
+            ew.notIn("id", recommendations);
+            ew.orderBy("id", false);
+            ew.last("limit "+toAddNum);
+            pageList.addAll(shangpinxinxiService.selectList(ew));
+        } else if(pageList.size()>limit) {
+            pageList = pageList.subList(0, limit);
+        }
+        page.setList(pageList);
+
+        return R.ok().put("data", page);
+    }
+
+
+
+
+
+
+
+
+}
